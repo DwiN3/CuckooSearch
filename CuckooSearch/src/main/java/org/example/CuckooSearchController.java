dc392b054ad83aeb3416873d265775e142e4bed3
DwiN3 <dwinicht333@gmail.com>
2023-04-25 12:22:02 +0200

    Przygotowanie projektu

--- CuckooSearch/src/main/java/org/example/CuckooSearchController.java
+++ CuckooSearch/src/main/java/org/example/CuckooSearchController.java
@@ -3,10 +3,8 @@ package org.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
-
+public class CuckooSearchController {
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
     }
 }
